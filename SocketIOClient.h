f9b6457833b49fb749e03cd05419010d73219075
quocbaodt <buiquocbaodt@gmail.com>
2017-10-17 08:37:35 +0700

    Them mot so tap tin moi

---
+++ SocketIOClient.h
@@ -0,0 +1,77 @@
+/*
+socket.io-arduino-client: a Socket.IO client for the Arduino
+Based on the Kevin Rohling WebSocketClient & Bill Roy Socket.io Lbrary
+Copyright 2015 Florent Vidal
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+JSON support added using https://github.com/bblanchon/ArduinoJson
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+#include "Arduino.h"
+#if defined(ESP32)
+#include <WiFi.h>					//For ESP32
+#elif (!defined(ESP32)	//If no interface is defined
+#error "Please specify an interface such as ESP32"
+#error "above your includes like so : #define ESP32 "
+#endif
+
+#define DEBUG
+#undef DEBUG 
+#define DATA_BUFFER_LEN 2048
+#define SID_LEN 24
+
+class SocketIOClient {
+public:
+	bool connect(char hostname[], int port = 80 , char nsp[] = "");
+
+	bool connectHTTP(char hostname[], int port = 80);
+	bool connected();
+	void disconnect();
+	bool reconnect(char hostname[], int port = 80 , char nsp[]= "");
+	bool monitor();
+	void sendMessage(String message = "");
+	void send(String RID, String Rname, String Rcontent);
+	void send(String RID, String JSON);
+	void sendNSP();
+	void sendJSON(String RID, String JSON);
+	void heartbeat(int select);
+	void clear();
+	void getREST(String path);
+	void postREST(String path, String type, String data);
+	void putREST(String path, String type, String data);
+	void deleteREST(String path);
+private:
+	void parser(int index);
+	void sendHandshake(char hostname[]);
+	//EthernetClient client;				//For ENC28J60 or W5100
+	WiFiClient client;				//For ESP8266 or ESP32
+	bool readHandshake();
+	void readLine();
+	char *dataptr;
+	char databuffer[DATA_BUFFER_LEN];
+	char sid[SID_LEN];
+	char key[28];
+	char *hostname;
+	char *nsp;
+	int port;
+
+	void findColon(char which);
+	void terminateCommand(void);
+	bool waitForInput(void);
+	void eatHeader(void);
+};
